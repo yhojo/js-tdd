2d022f220281f12af37d55981769272faae74f86
Yasuo Hojo <yhojo@pp.iij4u.or.jp>
2020-04-16 13:27:37 +0900

    インデントをスペースのみに変更した。

--- mycode-spec.js
+++ mycode-spec.js
@@ -1,9 +1,9 @@
 
 describe('足し算をする', function() {
-    it('1から10まで足したら55', function() {
-	expect(sumall(1,2,3,4,5,6,7,8,9,10)).toBe(55);
-    });
-    it('足す要素数を変えても計算できる', function () {
-	expect(sumall(1,2,3,4,5)).toBe(15);
-    });
+  it('1から10まで足したら55', function() {
+    expect(sumall(1,2,3,4,5,6,7,8,9,10)).toBe(55);
+  });
+  it('足す要素数を変えても計算できる', function () {
+    expect(sumall(1,2,3,4,5)).toBe(15);
+  });
 });
--- mycode.js
+++ mycode.js
@@ -1,8 +1,8 @@
 
 function sumall(/*arguments*/) {
-    var result = 0;
-    for (var i = 0, iend = arguments.length; i < iend; i++) {
-	result += arguments[i];
-    }
-    return result;
+  var result = 0;
+  for (var i = 0, iend = arguments.length; i < iend; i++) {
+    result += arguments[i];
+  }
+  return result;
 }
